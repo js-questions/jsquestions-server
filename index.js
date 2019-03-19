acc38ae4b1ccd4052004d17a5fd335fd76e15555
Julián González <jgpicatoste@gmail.com>
2019-03-19 20:54:06 +0100

    feat: WIP socket.io chat communicating with our backend fix: change koa-cors package

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 require('dotenv').config({ path: '.env' })
 
 const Koa = require('koa')
-const bodyParser = require('koa-bodyparser')
-const cors = require('koa-cors')
-
 const app = new Koa()
+const http = require('http').Server(app)
+
+const cors = require('@koa/cors')
+const bodyParser = require('koa-bodyparser')
 const db = require('./database/models')
 
-const Socketer = require('./socket')
+const io = require('socket.io')(http);
 
+io.on('connection', connection);
+
+const text = {
+  text: ''
+};
+
+function connection (socket) {
+  console.log('A new user with id ' + socket.id + ' has entered');
+  socket.emit('newUser', text);
+  socket.on('text', handleTextSend);
+
+  socket.on('join room', joinRoom)
+
+  // CHAT
+  socket.on('chat message', sendMsg);
+  function sendMsg (msg){
+    console.log(msg)
+    console.log(msg.room)
+    io.to(msg.room).emit('chat message', msg);
+  }
+
+  // EDITOR
+  function handleTextSend (data) {
+    console.log('handle text send', data);
+    text.text = data.text;
+    io.to('1234').emit('text', data); // It´s sending it to all the sockets, not only the ones in the room
+  }
+
+  function joinRoom(room) {
+    console.log('conected!', room)
+    socket.join(room);
+  }
+}
 
 app.use(bodyParser())
 app.use(cors());
@@ -17,8 +51,7 @@ app.use(cors());
   // await db.sequelize.drop();  // Testing DB
   // await db.sequelize.sync({ force: true });  // Testing DB
   await db.sequelize.sync()
-  app.listen(process.env.PORT, async () => {
+  http.listen(process.env.PORT, async () => {
     console.log(`Listening on port ${process.env.PORT}`)
-    const io = new Socketer(app)
   })
 })()
