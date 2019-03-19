ab3a8849eaafb220f19d91a6426e2664953813ee
Julián González <jgpicatoste@gmail.com>
2019-03-19 15:41:51 +0100

    feat: add socket.io first implementation as class fix: modify eslint dependencies and rules

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 require('dotenv').config({ path: '.env' })
 
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const cors = require('koa-cors');
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const cors = require('koa-cors')
 
-const app = new Koa();
+const app = new Koa()
 const db = require('./database/models')
 
-app.use(bodyParser());
+const Socketer = require('./socket')
+
+
+app.use(bodyParser())
 app.use(cors());
 
 (async () => {
   // await db.sequelize.drop();  // Testing DB
   // await db.sequelize.sync({ force: true });  // Testing DB
-  await db.sequelize.sync();
+  await db.sequelize.sync()
   app.listen(process.env.PORT, async () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-  });
+    console.log(`Listening on port ${process.env.PORT}`)
+    const io = new Socketer(app)
+  })
 })()
-
-console.log(typeof process.env.SALT);
