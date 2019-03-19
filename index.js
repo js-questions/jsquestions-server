3d50baf6bbacdbd6b92ecc7bbb0cca33f7d09ac9
Rodrigo Alcala <rodri.a90@gmail.com>
2019-03-19 11:20:48 +0100

    test: connect to sequelize tables

--- index.js
+++ index.js
@@ -11,21 +11,39 @@ app.use(bodyParser());
 app.use(cors());
 
 (async () => {
-  await db.sequelize.sync();
-  app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`)
+  await db.sequelize.drop();  // Testing DB
+  await db.sequelize.sync({ force: true });  // Testing DB
+  app.listen(process.env.PORT, async () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+    await db.User.create({  // Testing DB
+      username: 'Suelings',
+      email: 'test',
+      password: 'cha'
+    });
+
+    await db.User.create({  // Testing DB
+      username: 'test',
+      email: 'test2',
+      password: 'cha'
+    });
+
+    await db.Question.create({  // Testing DB
+      learner: 1,
+      title: 'test',
+      description: 'test',
+    })
+
+    await db.Offer.create({  // Testing DB
+      tutor: 2,
+      linkedQuestion: 1,
+    })
+
+    await db.Question.update({  // Testing DB
+      answeredBy: 1
+    }, {
+      where: { questionId: 1 }
+    })
   });
- })()
+})()
 
 console.log(typeof process.env.SALT);
-db.User.create({
-  username: 'Suelings',
-  email: 'test',
-  password: 'cha'
-});
-
-db.Question.create({
-  learner: 1,
-  title: 'test',
-  description: 'test',
-});
