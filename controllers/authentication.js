0193482193a3944f8c025a89715446d60f56c442
Rodrigo Alcala <rodri.a90@gmail.com>
2019-03-19 13:24:41 +0100

    feat: implement log-in controller and added it to the sign-up route

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,10 @@
 const db = require('./../database/models');
 const { Sequelize } = require('./../database/models');
+const btoa = require('btoa');
+const atob = require('atob');
+const bcrypt = require('bcrypt');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
 
 exports.signUp = async (ctx, next) => {
   const { username, email, password } = ctx.request.body;
@@ -34,5 +39,42 @@ exports.signUp = async (ctx, next) => {
     password
   })
 
+  // Set headers for log-in controller
+  const base64 = btoa(`${email}:${password}`);
+  ctx.headers.authorization = `Basic ${base64}`;
+
   await next();
+}
+
+exports.logIn = async (ctx) => {
+  if (ctx.headers.authorization) {
+    // Decode the Basic auth
+    const basic = ctx.headers.authorization.split(' ');
+    if (basic.length < 2 && basic[0] !== 'Basic') {
+      throw new Error('Missing basic authentication header.');
+    }
+    const [email, password] = atob(basic[1]).split(':');
+
+    // Check the user exists
+    const user = await db.User.findOne({ email });
+    if (!user) {
+      ctx.body = JSON.stringify('Invalid email or password');
+      return;
+    }
+
+    // Check the password is correct
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      console.log('No match', match);
+      ctx.body = JSON.stringify('Invalid email or password');
+      return;
+    }
+
+    // Generate the token and send it to the FE
+    const jwtSignAsync = promisify(jwt.sign);
+    ctx.body = {
+      token: await jwtSignAsync({ user }, process.env.JWTSECRET)
+    };
+    ctx.status = 200;
+  }
 }
--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const authCtrl = require('./controllers/authentication');
 
 const router = new Router();
 
-router.post('/sign-up', authCtrl.signUp);
-// router.get('/log-in', authCtrl.logIn);
+router.post('/sign-up', authCtrl.signUp, authCtrl.logIn);
+router.get('/log-in', authCtrl.logIn);
 
 module.exports = router;
