eb4de310f312e937b491b549aaf14f5242572846
Rodrigo Alcala <rodri.a90@gmail.com>
2019-03-19 20:38:50 +0100

    refactor: add try/catch statement to getAllUsers controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,23 @@
 exports.getAllUsers = async (ctx, db) => {
-  ctx.body = await db.User.findAll({
-    attributes: [
-      'userId',
-      'username',
-      'email',
-      'firstName',
-      'lastName',
-      'credits',
-      'karma',
-      'available',
-      'profileBadge',
-      'createdAt',
-      'updatedAt',
-    ]
-  });
-  ctx.status = 200;
+  try {
+    ctx.body = await db.User.findAll({
+      attributes: [
+        'userId',
+        'username',
+        'email',
+        'firstName',
+        'lastName',
+        'credits',
+        'karma',
+        'available',
+        'profileBadge',
+        'createdAt',
+        'updatedAt',
+      ]
+    });
+    ctx.status = 200;
+  } catch (err) {
+    console.log(err); // eslint-disable-line
+    ctx.status = 500;
+  }
 }
