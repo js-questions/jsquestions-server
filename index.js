d0fb36d3be0f1d622687717a74216ba271f93b69
Rodrigo Alcala <rodri.a90@gmail.com>
2019-03-19 11:28:12 +0100

    refactor: delete sequelize tests (working so far

--- index.js
+++ index.js
@@ -11,38 +11,11 @@ app.use(bodyParser());
 app.use(cors());
 
 (async () => {
-  await db.sequelize.drop();  // Testing DB
-  await db.sequelize.sync({ force: true });  // Testing DB
+  // await db.sequelize.drop();  // Testing DB
+  // await db.sequelize.sync({ force: true });  // Testing DB
+  await db.sequelize.sync();
   app.listen(process.env.PORT, async () => {
     console.log(`Listening on port ${process.env.PORT}`);
-    await db.User.create({  // Testing DB
-      username: 'Suelings',
-      email: 'test',
-      password: 'cha'
-    });
-
-    await db.User.create({  // Testing DB
-      username: 'test',
-      email: 'test2',
-      password: 'cha'
-    });
-
-    await db.Question.create({  // Testing DB
-      learner: 1,
-      title: 'test',
-      description: 'test',
-    })
-
-    await db.Offer.create({  // Testing DB
-      tutor: 2,
-      linkedQuestion: 1,
-    })
-
-    await db.Question.update({  // Testing DB
-      answeredBy: 1
-    }, {
-      where: { questionId: 1 }
-    })
   });
 })()
 
