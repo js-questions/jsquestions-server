c97adf38a83ea7a2c21cad9cc0c060a6f739350d
Julián González <jgpicatoste@gmail.com>
2019-03-20 17:24:02 +0100

    fix: fix typo in offer

--- controllers/offers.js
+++ controllers/offers.js
@@ -4,6 +4,18 @@ exports.createOffer = async (ctx, db) => {
 
   try {
 
+    const { message, expiration } = ctx.request.body;
+    const bearer = ctx.headers.authorization.split(' ');
+    const prettyBearer = (jwt.decode(bearer[1]));
+
+    ctx.body = await db.Offer.create({
+      tutor: prettyBearer.userId,
+      linkedQuestion: ctx.params.questionid,
+      message,
+      expiration
+    })
+    ctx.status = 200;
+
   } catch (err) {
     console.log(err); // eslint-disable-line
     ctx.status = 500;
