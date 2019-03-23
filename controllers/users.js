020db06c77bab313e8de6e489d1b41a70a001e23
Rodrigo Alcala <rodri.a90@gmail.com>
2019-03-23 16:33:43 +0100

    fix: implement new logic to asssign availability to /users endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -15,11 +15,11 @@ exports.getAllUsers = async (ctx, db) => {
         'updatedAt',
       ]
     });
-    for (let user in db.onlineUsers) {
-      if (db.onlineUsers[user]) {
-        users[user - 1].available = db.onlineUsers[user]
+    users.map(user => {
+      if (db.onlineUsers[user.user_id]) {
+        user.available = db.onlineUsers[user.user_id]
       }
-    }
+    })
     ctx.body = users;
     ctx.status = 200;
   } catch (err) {
